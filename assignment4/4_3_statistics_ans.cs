981798280fd4e1b991fffb0f0e3da31a8447ad6a
Jaeki Lee <xo0885544@gmail.com>
2025-05-07 01:24:20 +0900

    assign1,3

--- assignment4/4_3_statistics_ans.cs
+++ assignment4/4_3_statistics_ans.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 
+
 namespace statistics
 {
     class Program
@@ -20,9 +21,62 @@ namespace statistics
 
             int stdCount = data.GetLength(0) - 1;
             // ---------- TODO ----------
-            
-            // --------------------
+            int[] math = new int[stdCount];
+            int[] science = new int[stdCount];
+            int[] english = new int[stdCount];
+            int[] total = new int[stdCount];
+            string[] names = new string[stdCount];
+
+            for (int i = 1; i <= stdCount; i++)
+            {
+                names[i - 1] = data[i, 1];
+                math[i - 1] = int.Parse(data[i, 2]);
+                science[i - 1] = int.Parse(data[i, 3]);
+                english[i - 1] = int.Parse(data[i, 4]);
+                total[i - 1] = math[i - 1] + science[i - 1] + english[i - 1];
+            }
+
+
+            Console.WriteLine("Average Scores: ");
+            Console.WriteLine($"Math: {math.Average():0.00}");
+            Console.WriteLine($"Science: {science.Average():0.00}");
+            Console.WriteLine($"English: {english.Average():0.00}");
+
+            Console.WriteLine("\nMax and min Scores: ");
+            Console.WriteLine($"Math: ({math.Max()}, {math.Min()})");
+            Console.WriteLine($"Science: ({science.Max()}, {science.Min()})");
+            Console.WriteLine($"English: ({english.Max()}, {english.Min()})");
+
+
+            var totalsWithNames = names.Zip(total, (name, score) => new { name, score }).ToList();
+            var ranked = totalsWithNames
+                .OrderByDescending(x => x.score)
+                .Select((x, idx) => new { x.name, Rank = idx + 1 })
+                .ToDictionary(x => x.name, x => x.Rank);
+
+
+            Console.WriteLine("\nStudents rank by total scores:");
+            foreach (var name in names)
+            {
+                string suffix = GetRankSuffix(ranked[name]);
+                Console.WriteLine($"{name}: {ranked[name]}{suffix}");
+            }
         }
+
+
+        static string GetRankSuffix(int rank)
+        {
+            if (rank % 100 >= 11 && rank % 100 <= 13)
+                return "th";
+            switch (rank % 10)
+            {
+                case 1: return "st";
+                case 2: return "nd";
+                case 3: return "rd";
+                default: return "th";
+            }
+        }
+        // --------------------
     }
 }
 
